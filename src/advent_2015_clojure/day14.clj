8f4aff50915f9171238632d03c8a7795edd1e7bf
Andrew Byala <abyala@yahoo.com>
2022-01-08 10:54:17 -0600

    Day 14

--- src/advent_2015_clojure/day14.clj
+++ src/advent_2015_clojure/day14.clj
@@ -3,44 +3,40 @@
     [advent-2015-clojure.utils :refer [parse-long]]
     [clojure.string :as str]))
 
-(defn parse-reindeer [s]
-  (into {} (mapv vector
-                 [:speed :run-seconds :rest-seconds]
-                 (mapv parse-long (re-seq #"\d+" s)))))
-
-(defn parse-input [input]
-  (map parse-reindeer (str/split-lines input)))
+(defn parse-reindeer [s] (mapv parse-long (re-seq #"\d+" s)))
+(defn parse-input [input] (map parse-reindeer (str/split-lines input)))
 
 (defn reindeer-progress [reindeer]
   (letfn [(step-seq [[x & xs] n] (lazy-seq (cons n (step-seq xs (+ n x)))))]
-    (let [{:keys [speed run-seconds rest-seconds]} reindeer
+    (let [[speed run-seconds rest-seconds] reindeer
           steps (cycle (concat (repeat run-seconds speed)
                                (repeat rest-seconds 0)))]
       (step-seq steps 0))))
+
+(defn all-progress [reindeer]
+  (->> (map reindeer-progress reindeer)
+       (iterate (partial map rest))
+       (map (partial mapv first))))
 
 (defn lead-indexes [coll]
   (let [lead (reduce max coll)]
     (keep-indexed (fn [idx v] (when (= lead v) idx)) coll)))
 
-(defn take-turn [reindeer-stats]
-  (let [{:keys [steps scores]} reindeer-stats
-        next-distances (mapv first steps)
-        next-scores (reduce #(update %1 %2 inc)
-                            scores
-                            (lead-indexes next-distances))]
-    {:steps     (map rest steps)
-     :distances next-distances
-     :scores    next-scores}))
+(defn running-scores [position-seq]
+  (letfn [(score-seq [scores [positions & next-positions]]
+            (lazy-seq (cons scores
+                            (score-seq (reduce #(update %1 %2 inc)
+                                               scores
+                                               (lead-indexes positions))
+                                       next-positions))))]
+    (score-seq (mapv (constantly 0) (first position-seq)) (rest position-seq))))
 
-(defn race [all-reindeer]
-  (->> {:steps  (mapv (comp rest reindeer-progress) all-reindeer)
-        :scores (mapv (constantly 0) (range (count all-reindeer)))}
-       (iterate take-turn)
-       (map #(select-keys % [:distances :scores]))))
+(defn solve [input f]
+  (->> (parse-input input)
+       f
+       (drop 2503)
+       first
+       (reduce max)))
 
-(defn solve [attrib n input]
-  (let [result (nth (-> input parse-input race) n)]
-    (reduce max (attrib result))))
-
-(defn part1 [input] (solve :distances 2503 input))
-(defn part2 [input] (solve :scores 2503 input))
+(defn part1 [input] (solve input all-progress))
+(defn part2 [input] (solve input (comp running-scores all-progress)))
